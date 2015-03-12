4de86fa9080efaf2d0f9e6d4158239df6297dda4
webking <geniola.alberto@libero.it>
2015-03-12 15:28:08 +0100

    Added support for MSSQL 2008 R2

---
+++ JSONSource_100/OperationMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.webkingsoft.JSONSource_100
+{
+    public enum OperationMode
+    {
+        SyncIO,
+        StoreInMemory
+    }
+}
---
+++ JSONSource_100/VariableChooser.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace com.webkingsoft.JSONSource_100
+{
+    public partial class VariableChooser : Form
+    {
+        private Variables _vars;
+        private Variable _result;
+
+
+        public VariableChooser(Variables vars)
+        {
+            _vars = vars;
+            InitializeComponent();
+            DialogResult = DialogResult.Cancel;
+            varList.Items.Clear();
+
+            foreach (Variable v in _vars)
+            {
+                if (v.DataType == TypeCode.String)
+                {
+                    varList.Items.Add(v.QualifiedName);
+                }
+            }
+
+            if (varList.Items.Count > 0)
+                varList.SelectedIndex = 0;
+        }
+
+        private void VariableChooser_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            _result = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (varList.SelectedIndex != -1)
+            {
+                string cur = varList.Items[varList.SelectedIndex].ToString();
+                foreach (Variable v in _vars)
+                    if (v.QualifiedName == cur)
+                    {
+                        _result = v;
+                        DialogResult = DialogResult.OK;
+                        return;
+                    }
+            }
+
+            DialogResult = DialogResult.Cancel;
+            _result = null;
+
+        }
+
+        public Variable GetResult()
+        {
+            return _result;
+        }
+    }
+}
