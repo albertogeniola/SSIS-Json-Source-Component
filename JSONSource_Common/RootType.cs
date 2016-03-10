f8f331267ed813efb155abd1631c26c4aeab8233
Alberto Geniola <albertogenioa@gmail.com>
2016-03-10 19:04:56 +0200

    Dependency added

---
+++ JSONSource_Common/RootType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.webkingsoft.JSONSource_Common
+{
+    public enum RootType
+    {
+        JsonObject,
+        JsonArray
+    }
+}
